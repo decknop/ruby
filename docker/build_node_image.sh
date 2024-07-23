79bd407854a11feb25a2c1fd5ead1fc503055c7a
bryan <bryacasillas@gmail.com>
2024-07-22 23:51:32 -0600

    Auto building image script | change cmd command default

---
+++ docker/build_node_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+build_image() {
+    docker build -t node-image .
+    docker run -t -d -p 3000:3000 --name node-container node-image /bin/bash
+    docker exec -it node-container /bin/bash
+}
+
+OLD_IMAGE=$(docker images | grep node-image | awk '{print $1}')
+
+if [ $? == "0" ]; then
+    if docker ps -a | grep -q "node-container"; then
+        docker stop node-container
+        docker rm node-container
+    fi
+    docker rmi node-image:latest
+fi
+
+build_image
