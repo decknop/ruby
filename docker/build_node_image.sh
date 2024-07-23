ad9c24a5606ee056413c48ed121e695d70e96e1e
bryan <bryacasillas@gmail.com>
2024-07-23 00:20:39 -0600

    Change path to look the json file

--- docker/build_node_image.sh
+++ docker/build_node_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 build_image() {
-    cp ../config.json .
+    cp ../config/config.json .
     docker build -t node-image .
     docker run -t -d -p 3000:3000 --name node-container node-image /bin/bash
     docker exec -it node-container /bin/bash
