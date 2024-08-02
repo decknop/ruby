968d1e3884a951325da2e6f17bada9719108df7e
Bryan Casillas <66758876+Bryancdi@users.noreply.github.com>
2024-08-01 20:33:37 -0600

    Update build_node_image.sh

--- ruby/docker/build_node_image.sh
+++ ruby/docker/build_node_image.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# NAME_IMAGE=$1
+NAME_IMAGE=$1
 
-# if [ "$#" != 1 ]; then
-#     echo "# of arguments missing, need to type the name that you would like for image and container"
-#     exit 0
-# fi
+if [ "$#" != 1 ]; then
+    echo "# of arguments missing, need to type the name that you would like for image and container"
+    exit 0
+fi
 
-# build_image() {
-#     cp ../config.json .
-#     docker build -t $NAME_IMAGE-image .
-#     docker run -t -d -p 3000:3000 --name $NAME_IMAGE-container $NAME_IMAGE-image /bin/bash
-#     docker exec -it $NAME_IMAGE-container /bin/bash
-# }
+build_image() {
+    # cp ../config.json .
+    docker build -t $NAME_IMAGE-image .
+    docker run -t -d -p 3000:3000 --name $NAME_IMAGE-container $NAME_IMAGE-image /bin/bash
+    docker exec -it $NAME_IMAGE-container /bin/bash
+}
 
-# OLD_IMAGE=$(docker images | grep $NAME_IMAGE-image | awk '{print $1}')
+OLD_IMAGE=$(docker images | grep $NAME_IMAGE-image | awk '{print $1}')
 
-# if [ $? == "0" ]; then
-#     if docker ps -a | grep -q "$NAME_IMAGE-container"; then
-#         docker stop $NAME_IMAGE-container
-#         docker rm $NAME_IMAGE-container
-#     fi
-#     docker rmi $NAME_IMAGE-image:latest
-# fi
+if [ $? == "0" ]; then
+    if docker ps -a | grep -q "$NAME_IMAGE-container"; then
+        docker stop $NAME_IMAGE-container
+        docker rm $NAME_IMAGE-container
+    fi
+    docker rmi $NAME_IMAGE-image:latest
+fi
 
-# build_image
+build_image
 
-echo "You got the docker script run!"
+# echo "You got the docker script run!"
